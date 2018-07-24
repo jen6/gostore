ed819ded2864c150058afa345dd4f2830528518b
SonGeon <gungun.son@navercorp.com>
2018-07-24 12:59:05 +0900

    basic scrapper ok

---
+++ scrapper.go
@@ -0,0 +1,86 @@
+package gostore
+
+import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	gq "github.com/PuerkitoBio/goquery"
+	"io"
+	"net/http"
+	"strings"
+	"time"
+)
+
+func GetNewApps() (io.ReadCloser, error) {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}
+
+	var client = http.Client{
+		Timeout:   10 * time.Second,
+		Transport: tr,
+	}
+
+	reqForm := map[string]interface{}{
+		"start":       0,
+		"num":         100,
+		"numChildren": 0,
+		"cctcss":      "sqare-cover",
+		"cllayout":    "NORMAL",
+		"ipf":         1,
+		"xhr":         1,
+	}
+
+	jsonData, _ := json.Marshal(reqForm)
+	buffer := bytes.NewBuffer(jsonData)
+
+	req, err := http.NewRequest("POST", "https://play.google.com/store/apps/collection/topselling_new_free?hl=ko&authuser=0", buffer)
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	return resp.Body, nil
+}
+
+type AppInfo struct {
+	AppName string
+	PkgName string
+}
+
+func GetAppList(body io.Reader) []AppInfo {
+	doc, err := gq.NewDocumentFromReader(body)
+	if err != nil {
+		fmt.Println(err)
+		return []AppInfo{}
+	}
+
+	var result []AppInfo
+	sel := doc.Find("div .details > .title")
+
+	sel.Each(func(i int, s *gq.Selection) {
+		url, ok := s.Attr("href")
+		if !ok {
+			return
+		}
+		pkgName := strings.Split(url, "=")[1]
+		appName, ok := s.Attr("title")
+		if !ok {
+			return
+		}
+
+		result = append(result, AppInfo{AppName: appName, PkgName: pkgName})
+	})
+
+	return result
+}
---
+++ scrapper_test.go
@@ -0,0 +1,42 @@
+package gostore
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestGetNewApps(t *testing.T) {
+	_, err := GetNewApps()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+type InfoTest struct {
+	Data     string
+	AppNames []string
+	PkgNames []string
+}
+
+func TestGetAppList(t *testing.T) {
+	tests := []InfoTest{
+		{
+			Data:     `<div class="details"> <a class="card-click-target" href="/store/apps/details?id=com.papegames.evol.kr.release" aria-hidden="true" tabindex="-1"> </a> <a class="title" href="/store/apps/details?id=com.papegames.evol.kr.release" title="러브앤프로듀서" aria-hidden="true" tabindex="-1"> 1. 러브앤프로듀서 <span class="paragraph-end"></span> </a> <div class="subtitle-container"> <a class="subtitle" href="/store/apps/developer?id=Paper+Games" title="Paper Games">Paper Games</a> <span class="price-container"> <span class="paragraph-end"></span> </span> </div><div class="description"> 너의 마음을 두드릴 초능력! <span class="paragraph-end"></span> <a class="card-click-target" href="/store/apps/details?id=com.papegames.evol.kr.release" aria-hidden="true" tabindex="-1"> </a> </div></div>`,
+			AppNames: []string{"러브앤프로듀서"},
+			PkgNames: []string{"com.papegames.evol.kr.release"},
+		},
+	}
+
+	for _, testData := range tests {
+		reader := strings.NewReader(testData.Data)
+		infos := GetAppList(reader)
+		for i, info := range infos {
+			if testData.AppNames[i] != info.AppName {
+				t.Error("Expact : ", testData.AppNames[i], " Value : ", info.AppName)
+			}
+			if testData.PkgNames[i] != info.PkgName {
+				t.Error("Expact : ", testData.PkgNames[i], " Value : ", info.PkgName)
+			}
+		}
+	}
+}
